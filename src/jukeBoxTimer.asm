8de0a1571591482d10bc467c971cbc884ea54da7
raf11111 <potwor.z.javy@gmail.com>
2014-02-23 15:26:09 -0800

    fixed timer, added subroutine to set tune len

--- src/jukeBoxTimer.asm
+++ src/jukeBoxTimer.asm
@@ -1,5 +1,10 @@
 SUBROUTINE jukeBoxTimer
 
+setTimerTuneLengthAY ; A = lo, Y = hi
+	sta musicTimeLo
+	sty musicTimeHi
+	rts
+
 checkTimer
 	lda musicTimeLo
 	cmp musicTimerLo
@@ -8,31 +13,38 @@ checkTimer
 	cmp musicTimerHi
 	bne .skip2
 	
-	; TIMER HIT HERE
-	inc $d020
+	; TIMER HIT 
+	inc tuneToLoad ; TODO find a smarter way to switch the tune - this is ugly hack
+	lda #OPLoadTune
+	sta OPFLAG
 	
 .skip2
 	rts
 
 incTimer
-	inc musicTimerLo
+	clc
+	lda musicTimerLo
+	adc #1
+	sta musicTimerLo
 	bcc .skip1
 	inc musicTimerHi
 .skip1
 	rts
 
+; clock value
+musicTimer
+musicTimerLo
+.byte $00
+musicTimerHi
+.byte $00	
+	
 resetTimer
 	lda #0
 	sta musicTimer
 	sta musicTimer + 1
 	rts
 
-musicTimer
-musicTimerLo
-.byte $00
-musicTimerHi
-.byte $00
-
+; tune len
 musicTimeLo
 .byte $00
 musicTimeHi
