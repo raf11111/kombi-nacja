6db9ea23297a8d8f71675f392ec7c61769cf53bf
raf11111 <potwor.z.javy@gmail.com>
2014-02-23 13:40:24 -0800

    added support for up/dn scrolling menu (not finished, partailly functional)

--- src/kbdirq.asm
+++ src/kbdirq.asm
@@ -1,3 +1,5 @@
+SUBROUTINE checkKbdIRQ
+
 checkkbd: 
 ;fe ins/del
 ;fd return
@@ -23,33 +25,49 @@ checkkbd:
 		cmp #$ff
 		beq ckeckkbdexit
 
-		sta $c000+24*40
+		sta $c000+13*40
 		jsr checkKeys
 		
 ckeckkbdexit
 		lda tuneToLoad
-		sta $c001+24*40
+		sta $c001+13*40
 		rts
 
-checkKeys:		
+checkKeys:	
+;left-right
 __f1    cmp #$fb
 		bne __f3
 		ldx tuneToLoad
-		beq *+3
+		beq __f3
 		dec tuneToLoad
 		inc $d020
 		jsr moveScreenUp
+		
+		lda #<($c000 + 24*40)
+		sta .dstLn + 1
+		lda #>($c000 + 24*40)
+		sta .dstLn + 2
+		jsr printNewSong
+				
 		dec $d020
 		rts
-		
+
+;up-dn
 __f3	cmp #$7f
 		bne __f5
 		ldx tuneToLoad
 		cpx #NUMBER_OF_TUNES
-		bpl *+3
+		bpl __f5
 		inc tuneToLoad
 		inc $d020
 		jsr moveScreenDown
+
+		lda #<($c000 + 16*40)
+		sta .dstLn + 1
+		lda #>($c000 + 16*40)
+		sta .dstLn + 2
+		jsr printNewSong
+
 		dec $d020
 		rts
 		
@@ -61,3 +79,24 @@ exitkbd
 		rts
 		
 lastkey byte 0		
+
+printNewSong:
+		jsr mul40
+		
+		lda mul40hi
+		clc
+		adc #>menuItems
+		sta .srcChr + 2
+		
+		lda mul40lo
+		clc
+		adc #<menuItems
+		sta .srcChr + 1
+		
+		ldy #39
+.srcChr	lda $0000,y
+.dstLn	sta $0000,y
+		dey
+		bpl .srcChr
+
+		rts
